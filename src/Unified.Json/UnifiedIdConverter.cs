f2a0ff2b36b7702e29662053af0fc7f6a5514227
Sergiy Seletsky <SeletskySergey@live.com>
2020-09-21 23:56:20 +0300

    added json converter

---
+++ src/Unified.Json/UnifiedIdConverter.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace Unified.Json
+{
+    /// <summary>
+    /// UnifiedIdConverter.
+    /// </summary>
+    public class UnifiedIdConverter : JsonConverter
+    {
+        /// <inheritdoc/>
+        public override bool CanConvert(Type objectType)
+        {
+            return objectType == typeof(UnifiedId);
+        }
+
+        /// <inheritdoc/>
+        public override object ReadJson(JsonReader reader, Type objectType, object? existingValue, JsonSerializer serializer)
+        {
+            return UnifiedId.Parse(JToken.Load(reader).ToString());
+        }
+
+        /// <inheritdoc/>
+        public override void WriteJson(JsonWriter writer, object? value, JsonSerializer serializer)
+        {
+            if (value == null)
+            {
+                throw new ArgumentNullException(nameof(value));
+            }
+
+            JToken.FromObject(value.ToString()).WriteTo(writer);
+        }
+    }
+}
