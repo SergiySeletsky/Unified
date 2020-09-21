773e116f3e06c8a268a3c0bab5e9c5d4723ea373
Sergey Seletsky <SeletskySergey@live.com>
2020-09-21 09:52:36 +0300

    fix tests

--- test/Unified.Tests/PartitioningTests.cs
+++ test/Unified.Tests/PartitioningTests.cs
@@ -15,7 +15,7 @@ namespace Unified.Tests
         }
 
         [Fact]
-        public void TestPartition()
+        public void PartitionNumberTest()
         {
             var dict = new Dictionary<ulong, List<UnifiedId>>();
             var all = 100000;
@@ -25,6 +25,74 @@ namespace Unified.Tests
             {
                 var x = UnifiedId.NewId();
                 var p = x.PartitionNumber(partitions);
+                if (!dict.ContainsKey(p))
+                {
+                    dict.Add(p, new List<UnifiedId>());
+                }
+
+                dict[p].Add(x);
+            }
+
+            float max = dict.Max(x => x.Value.Count);
+            float min = dict.Min(x => x.Value.Count);
+
+            var diff = max - min;
+
+            var maxp = (max / (all / partitions)) * 100;
+            var minp = (min / (all / partitions)) * 100;
+            var diffp = (diff / (all / partitions)) * 100;
+
+            output.WriteLine($"Elements tested: {all} partition size: {all / partitions}");
+            output.WriteLine($"MAX: {max:N1}({maxp:N1}%) MIN: {min:N1}({minp:N1}%) DIFF: {diff:N1}({diffp:N1}%)");
+
+            Assert.True(diffp < 10);
+        }
+
+        [Fact]
+        public void PartitionNumberStringTest()
+        {
+            var dict = new Dictionary<string, List<UnifiedId>>();
+            var all = 100000;
+
+            var partitions = 16U;
+            for (var i = 0; i <= all; i++)
+            {
+                var x = UnifiedId.NewId();
+                var p = x.PartitionNumberAsString(partitions);
+                if (!dict.ContainsKey(p))
+                {
+                    dict.Add(p, new List<UnifiedId>());
+                }
+
+                dict[p].Add(x);
+            }
+
+            float max = dict.Max(x => x.Value.Count);
+            float min = dict.Min(x => x.Value.Count);
+
+            var diff = max - min;
+
+            var maxp = (max / (all / partitions)) * 100;
+            var minp = (min / (all / partitions)) * 100;
+            var diffp = (diff / (all / partitions)) * 100;
+
+            output.WriteLine($"Elements tested: {all} partition size: {all / partitions}");
+            output.WriteLine($"MAX: {max:N1}({maxp:N1}%) MIN: {min:N1}({minp:N1}%) DIFF: {diff:N1}({diffp:N1}%)");
+
+            Assert.True(diffp < 10);
+        }
+
+        [Fact]
+        public void PartitionKeyTest()
+        {
+            var dict = new Dictionary<string, List<UnifiedId>>();
+            var all = 100000;
+
+            var partitions = 16U;
+            for (var i = 0; i <= all; i++)
+            {
+                var x = UnifiedId.NewId();
+                var p = x.PartitionKey(1);
                 if (!dict.ContainsKey(p))
                 {
                     dict.Add(p, new List<UnifiedId>());
